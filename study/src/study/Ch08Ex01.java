8bda07e6d91b858507d9f170830c41a1af6886c7
tifa93 <hooni93@me.com>
2020-03-19 21:05:32 +0800

    test

---
+++ study/src/study/Ch08Ex01.java
@@ -0,0 +1,13 @@
+package study;
+
+public class Ch08Ex01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Point pt01 = new Point(10,20);
+		pt01.showPoint();
+		pt01.setX(30);
+		pt01.showPoint();
+	}
+
+}
---
+++ study/src/study/testMain.java
@@ -0,0 +1,52 @@
+package study;
+
+public class testMain {
+	public static void main(String[] args) {
+		//정수
+		int jungsoo = 1;
+		int jungsoo2 = 1;
+		long l = 1;
+		byte b = 1;
+		short s = 1;
+		
+		//실수
+		double silsoo = 1.0;
+		float slisoo2 = 1.1f;
+		
+		//문자
+		String st =  "쭈꾸미 오빠랑";
+		char c = '문';
+		
+		//연산
+		int sum = (jungsoo + jungsoo2) *3;
+		
+		//논리
+		boolean bbb = false;
+		
+		if(bbb==true) {
+			
+		} else if (bbb==false) {
+			
+		} else {
+			
+		}
+		
+		//반복문
+		for (int i = 0 ; i < 10 ; i++) {
+			
+		}
+		
+		int[] arrayA = new int[5];
+		for (int i : arrayA) {
+			
+		}
+		
+		while(bbb == true) {
+			
+		}
+		
+		do {
+			
+		} while(bbb == true);
+	}
+}
